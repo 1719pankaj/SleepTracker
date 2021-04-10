7105fd309b95de2542b1e93e4eb85d3c0bc3eab7
Pankaj Kumar Roy <1719pankaj@gmail.com>
2021-04-10 14:19:20 +0530

    L06C13 - Adding a ViewModel

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,38 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import android.provider.ContactsContract
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class], version = 1,exportSchema = false)
+        abstract class SleepDatabase : RoomDatabase() {
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object{
+
+                @Volatile  //Optional maybe but makes sure no traffic accidents happen
+                private var  INSTANCE: SleepDatabase? = null
+
+                fun getInstance(context: Context) : SleepDatabase {
+                    synchronized(this) {
+                        var instance = INSTANCE
+                        if (instance == null) {
+                            instance = Room.databaseBuilder(
+                                    context.applicationContext,
+                                    SleepDatabase::class.java,
+                                    "sleep_history_database"
+                            )
+                                    .fallbackToDestructiveMigration()
+                                    .build()
+                            INSTANCE = instance
+                        }
+
+                        return instance
+                    }
+                }
+            }
+        }
